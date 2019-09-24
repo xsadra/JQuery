e3d9c7c4d2608207c69a070ea30b926338e5cc30
Sadra <31988724+xsadra@users.noreply.github.com>
2019-09-24 19:32:37 +0200

    Traversing Find

---
+++ 04-01-traversing-find/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	//CSS Selector
+	var selected = $('#animals .creator');
+	//console.log(selected); 
+
+	//DOM traversing
+	result = $('#animals').find('.creature');
+	console.log(result);
+
+
+
+});
