41cf07bb6eb5e2cca258d0df87c65e3468162319
Sadra <31988724+xsadra@users.noreply.github.com>
2019-11-25 19:57:56 +0100

    Traversing Parents

---
+++ 04-04-traversing-parents/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	// parent()
+	var result = $('#cat').parent();
+	console.log(result);
+	
+	// parents()
+	result = $('#cat').parents();
+	// result = $('#cat').parents('.category');
+	console.log(result);
+
+	// closest()
+	result = $('#cat').closest('.category');
+	console.log(result);
+	
+});
